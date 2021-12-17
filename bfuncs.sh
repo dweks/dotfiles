5654eb7681765006eb356a38a80d4dd6a8656e6b
Blake Stephens <sqthreer@gmail.com>
2021-12-17 11:31:02 -0800

    added red to missing commit message error message

--- bfuncs.sh
+++ bfuncs.sh
@@ -7,7 +7,7 @@ function pulldot()
 function pushdot()
 {
 	if [ -z $1 ]; then
-		echo "Provide commit message"
+		echo -e "\e[1;31mProvide commit message\e[m"
 		return
 	fi	
 	COMMIT=$@
@@ -16,6 +16,7 @@ function pushdot()
 	git -C $DOTFILES add .
 	echo -e "\e[0;33mCommitting: \"$COMMIT\" \e[m"
 	git -C $DOTFILES commit -m "$COMMIT"
+	git -C $DOTFILES status
 	echo -e "\e[0;33mPushing to GitHub...\e[m"
 	git -C $DOTFILES push
 }
